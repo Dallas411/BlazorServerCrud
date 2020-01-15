ca033e6f7b344c09ddcebb9ad3387931215a3679
Marco Dalla Libera <marco@dallalibera.net>
2020-01-15 09:38:00 +0100

    Submit Forms con evento standard ed altre amenità

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorServerCrud.Models
 {
     public partial class Employee
     {
         public int EmployeeId { get; set; }
+
+        [StringLength(20,MinimumLength = 3)]
         public string Name { get; set; }
         public string City { get; set; }
         public string Department { get; set; }
